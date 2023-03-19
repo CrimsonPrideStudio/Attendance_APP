aff48e1dfaa05750d375402f0d27d8a36960be69
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2023-03-19 21:01:30 +0530

    ViewPager2 is Completed

--- app/src/main/java/com/example/attendance_app/Fragment/Test.kt
+++ app/src/main/java/com/example/attendance_app/Fragment/Test.kt
@@ -1,26 +1,29 @@
 package com.example.attendance_app.Fragment
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.example.attendance_app.Adaptar.ViewPagerAdapter
 import com.example.attendance_app.R
 import com.sagarkoli.chetanbottomnavigation.chetanBottomNavigation
-import kotlinx.android.synthetic.main.activity_main.*
 
 class Test : AppCompatActivity() {
-    val fragments = listOf(Profile(), Dashboard(), Setting())
 
     lateinit var navBar: chetanBottomNavigation
+    lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navBar = findViewById(R.id.navBar)
+        viewPager = findViewById(R.id.viewPager)
 
-        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val fragments = listOf(Profile(), Dashboard(), Setting())
         viewPager.adapter = ViewPagerAdapter(this, fragments)
-
-        navBar=findViewById(R.id.navBar)
 
         navBar.add(chetanBottomNavigation.Model(1,R.drawable.profile))
         navBar.add(chetanBottomNavigation.Model(2,R.drawable.dashboard_icon))
@@ -44,24 +47,19 @@ class Test : AppCompatActivity() {
 
         }
 
-        navBar.show(2,true)
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                navBar.show(position + 1, true)
+            }
+        })
 
+        viewPager.currentItem=1
+
+        navBar.show(2,true)
     }
-    //private fun changeFragment(fragment: Fragment){
-    //    val fragManager=supportFragmentManager
-    //    val fragTransaction=fragManager.beginTransaction()
-    //    fragTransaction.replace(R.id.fragContainer,fragment)
-    //    fragTransaction.commit()
-    //}
 
     private fun changeFragment(position: Int) {
-        val fragManager=supportFragmentManager
-        val fragTransaction=fragManager.beginTransaction()
-        fragTransaction.replace(R.id.fragContainer,fragments[position])
-        fragTransaction.commit()
         viewPager.currentItem = position
-
     }
-
 }
 
